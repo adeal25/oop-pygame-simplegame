db810d6cc4dae6df4744b279e94297bb50e514f4
adeal25 <103437464+adeal25@users.noreply.github.com>
2022-04-11 22:51:05 +0700

    Create level3.py

---
+++ oop/level/level3.py
@@ -0,0 +1,29 @@
+import pygame
+
+class Level3():
+    ''' Level 1 - berlatar belakang angkasa '''
+
+    def setup(self, screen, resolution):
+        print(resolution)
+        self.screen = screen
+        self.resolution = resolution
+    
+        self.background_sound()
+    
+
+    def background_sound(self, volume=0.25):
+        pygame.mixer.init()
+        pygame.mixer.music.load("resources/audio/onthemoon.wav")
+        pygame.mixer.music.play(-1, 0.0)
+        pygame.mixer.music.set_volume(volume)
+
+    def tiles(self):
+        return pygame.image.load('resources/images/galaxy.png')
+
+    def draw(self):        
+        for x in range(int(self.resolution[0]/self.tiles().get_width()+1)):
+            for y in range(int(self.resolution[1]/self.tiles().get_height()+1)):
+                self.screen.blit(
+                    self.tiles(), (x*self.tiles().get_width(), y*self.tiles().get_height()))
+        
+    
